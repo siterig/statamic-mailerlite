501261792218fa1d21516e7f1994a155002fd110
Matt Stone <matt@rockandscissor.com>
2021-04-19 07:48:58 +0100

    Added merge and publish of config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,9 +3,9 @@
 namespace SiteRig\MailerLite;
 
 use Edalzell\Forma\Forma;
-use SiteRig\MailerLite\Http\Controllers\ConfigController;
 use SiteRig\MailerLite\Fieldtypes\SubscriberField;
 use SiteRig\MailerLite\Fieldtypes\SubscriberGroup;
+use SiteRig\MailerLite\Http\Controllers\ConfigController;
 use Statamic\Events\SubmissionCreated;
 use Statamic\Facades\User;
 use Statamic\Providers\AddonServiceProvider;
@@ -21,6 +21,11 @@ class ServiceProvider extends AddonServiceProvider
     public function boot()
     {
         parent::boot();
+
+        $this->mergeConfigFrom(__DIR__ . '/../config/mailerlite.php', 'mailerlite');
+        $this->publishes([
+            __DIR__ . '/../config/mailerlite.php' => config_path('mailerlite.php'),
+        ], 'config');
 
         $this->app->booted(function () {
             Forma::add('siterig/mailerlite', ConfigController::class);
